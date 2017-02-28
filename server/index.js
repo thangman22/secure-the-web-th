27eca64e7a7346baed5a548b6ff2f1a91f90838a
warat wongmaneekit <warat@zocialinc.com>
2017-02-28 10:34:28 +0700

    Add submision campaign

--- server/index.js
+++ server/index.js
@@ -1,5 +1,34 @@
-var ssllabs = require("node-ssllabs");
+var ssllabs = require('node-ssllabs');
+var request = require('request')
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream('top100.list')
+})
 
-ssllabs.scan("zocialeye.com", function (err, host) {
-	console.dir(host);
-});
+lineReader.on('line', function (line) {
+  request('https://'+line, function (error, response, body) {
+    if(response){
+
+      if(response.statusCode === 200){
+        var secureStatue = ',Secure'
+      }
+
+      request('http://'+line, function (error, response, body) {
+        uri = JSON.parse(JSON.stringify(response.request.uri))
+        if(uri.port === 443){
+          var forceStatue = ',Force HTTPS'
+        }else{
+          var forceStatue = ',Not Force HTTPS'
+        }
+
+        console.log(line+' '+secureStatue+' '+forceStatue)
+      })
+    }else{
+      console.log(line+',Not Secure')
+    }
+
+  })
+})
+
+// ssllabs.scan('zocialeye.com', function (err, host) {
+// 	console.dir(host);
+// });
